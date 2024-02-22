ae5760208f226e7a0ef0dc3b301c514e6c09674a
KarlMarkusVahe <karl-markus.vaher@voco.ee>
2024-02-22 10:47:04 +0200

    Checkbox

--- src/screens/auth/SignUp/index.js
+++ src/screens/auth/SignUp/index.js
@@ -1,15 +1,23 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import AuthHeader from "../../../components/AuthHeader";
 import Input from "../../../components/Input"
 import styles from "./styles";
+import Checkbox from "../../../components/Checkbox";
+import { useState } from "react";
 
 const Signup = () => {
+    const [checked, setChecked] = useState(false)
+
     return (
         <View style={styles.container}>
             <AuthHeader title="Sign Up"></AuthHeader>
             <Input label="Name" placeholder="John Doe"></Input>
             <Input label="Email" placeholder="example@gmail.com"></Input>
             <Input isPassword label="Password" placeholder="******"></Input>
+            <View style={styles.agreeRow}>
+                <Checkbox checked={checked} onCheck={setChecked}></Checkbox>
+                <Text style={styles.agreeText}>I agree with Terms & Privacy</Text>
+            </View>
         </View>
     )
 }
--- src/screens/auth/SignUp/styles.js
+++ src/screens/auth/SignUp/styles.js
@@ -1,7 +1,15 @@
 import { StyleSheet } from "react-native";
-import { colors } from "../../utils/colors";
+import { colors } from "../../../utils/colors";
 export default styles = StyleSheet.create({
     container: {
         padding: 24
+    },
+    agreeRow: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    agreeText: {
+        color: colors.blue,
+        marginHorizontal: 14
     }
 })
