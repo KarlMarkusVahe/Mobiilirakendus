d89f355d9b728ea926bc78cfbc3f9659aa6a949c
KarlMarkusVahe <karl-markus.vaher@voco.ee>
2024-03-01 10:06:47 +0200

    ProductDetails footer

--- src/screens/App/ProductDetails/index.js
+++ src/screens/App/ProductDetails/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
+import Button from '../../../components/Button'
 
 const ProductDetails = ({navigation, route}) => {
     const {product} = route.params || {}
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.save}>
             <ScrollView>
                 <Image style={styles.image} source={{uri: product?.image}}></Image>
                 <View style={styles.content}>
@@ -15,6 +16,12 @@ const ProductDetails = ({navigation, route}) => {
                     <Text style={styles.description}>{product?.description}</Text>
                 </View>
             </ScrollView>
+            <View style={styles.footer}>
+                <Pressable style={styles.bookmarkContainer}>
+                    <Image style={styles.bookmarkIcon} source={require('../../../assets/tabs/Favo.png')}></Image>
+                </Pressable>
+                <Button title="Contact Seller"></Button>
+            </View>
         </SafeAreaView>
     )
 }
